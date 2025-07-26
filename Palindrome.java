dc7cfc3b4e2c561428af099cb552f500f63b7aec
sindhujagathees <155611392+sindhujagathees@users.noreply.github.com>
2025-07-26 21:46:00 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,31 @@
+package week1.day2;
+
+public class Palindrome {
+
+			    public static void main(String[] args)
+		    {
+		        int input = 121; 
+		        int output = 0;
+		        int originalInput = input;
+
+		        
+		        for (int i = input; i > 0; i = i / 10) {
+		            int rem = i % 10; 
+		            output = output * 10 + rem; 
+		        }
+
+		      
+		        if (originalInput == output) {
+		            System.out.println("It is a Palindrome");
+		        } else {
+		            System.out.println("It is not a Palindrome");
+		        }
+		    }
+		
+
+
+		// TODO Auto-generated method stub
+
+	}
+
+

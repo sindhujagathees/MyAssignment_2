970c630c651fb7c29bdd7b28bff5c76b10a0388b
sindhujagathees <155611392+sindhujagathees@users.noreply.github.com>
2025-07-26 21:45:29 +0530

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,24 @@
+package week1.day2;
+
+public class Library
+{
+	public void addBook() {
+		System.out.println("Book added successfully");
+	}
+	
+    public void issueBook() {
+    	System.out.println("Book issued successfully");
+    }
+    public String bookTitle()
+    {
+    	return "bookTitle";
+    }
+	public static void main(String[] args) {
+		Library books=new Library();
+		books.addBook();
+		books.issueBook();
+		String bookTitle=books.bookTitle();
+		System.out.println(bookTitle);
+			}
+
+}
---
+++ LibraryManagement.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		Library books=new Library();
+		books.addBook();
+		books.issueBook();
+		
+
+	}
+
+}
